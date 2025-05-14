d74c77c53de13910c19716303c53f7595fd6f5ff
Igor Otero Muniz <109098168+IgorOtr@users.noreply.github.com>
2025-05-13 22:35:06 -0300

    dnjhebdjoew

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
-// Serve imagens da pasta de uploads
 app.use('/uploads', express.static(path.join(__dirname, '..', 'public/uploads')));
-
-// Rotas da aplicação
 app.use(userRoutes);
-
-// Conecta e sincroniza com banco de dados
 sequelize.authenticate()
   .then(() => console.log('Conectado ao MySQL'))
   .catch(err => console.error('Erro de conexão:', err));
